2b75eb1df3e5e990e2510a7d184730b62120144e
rastylus <ryan.p.riggs@gmail.com>
2023-10-24 04:47:44 -0500

    added persistent tags

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,14 +1,14 @@
 package org.launchcode.codingevents.models;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Chris Bay
@@ -29,6 +29,9 @@ public class Event extends AbstractEntity {
     @Valid
     @NotNull
     private EventDetails eventDetails;
+
+    @ManyToMany
+    private final List<Tag> tags = new ArrayList<>();
 
     public Event(String name, EventCategory eventCategory) {
         this.name = name;
@@ -60,6 +63,14 @@ public class Event extends AbstractEntity {
 
     public void setEventDetails(EventDetails eventDetails) {
         this.eventDetails = eventDetails;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
     }
 
     @Override
